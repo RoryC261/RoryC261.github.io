bbc17c5cb22042d31c311cc167c78e866db14fd9
roryc261 <rorycameron75@gmail.com>
2020-12-01 14:34:56 +0000

    Calums code added

---
+++ Events & Members JS.js
@@ -0,0 +1,14 @@
+//Catchphrase buttons
+function myFunction() {
+    document.getElementById("demo").innerHTML = '"WAAAAAAAAHHHH!"';
+}
+function myFunction2() {
+    document.getElementById("j2").innerHTML = '"I used to be an adventurere like you, till I took an arrow to the knee"';
+}
+function myFunction3() {
+    document.getElementById("j3").innerHTML = '"Blue is sus"'; 
+}
+function displayDate() {
+    document.getElementById("date").innerHTML = Date();
+  }
+  
